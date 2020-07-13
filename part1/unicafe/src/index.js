fde2906371cc1d377223d877cbc40fceb5b6041d
John MacLellan <johnmaclellan@Johns-MacBook-Pro.local>
2020-07-12 21:12:19 -0400

    Add buttons and event handlers

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <h3>good: {good}</h3>
+      <h3>neutral: {neutral}</h3>
+      <h3>bad: {bad}</h3>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
