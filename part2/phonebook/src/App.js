5b8d40f83d55c0b5a5938ba630480fed8b4aa621
John MacLellan <johnmaclellan@Johns-MacBook-Pro.local>
2020-07-14 22:33:10 -0400

    Started filter

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '123-456-7890' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const addName = e => {
+  const addPerson = e => {
     e.preventDefault()
     const newPerson = {
       name: newName,
+      number: newNumber,
       key: newName
     }
 
@@ -25,22 +27,32 @@ const App = () => {
     setNewName(e.target.value)
   }
 
+  const handleNumberChange = e => {
+    setNewNumber(e.target.value)
+  }
+
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <h1>Phonebook</h1>
+      {/* <div>filter shown with <input value={} onChange={} /></div> */}
+      <h2>add a new</h2>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input
-            value={newName}
-            onChange={handleNameChange}
-          />
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.key}>{person.name}</div>)}
+      {persons.map(person =>
+        <div key={person.key}>
+          {person.name} {person.number}
+        </div>
+      )}
     </div>
   )
 }
