38f7d4f57bd7413fa5bf457466fba343fed02442
John MacLellan <johnmaclellan@Johns-MacBook-Pro.local>
2020-11-28 14:26:05 -0500

    fixed comment

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -70,7 +70,6 @@ const App = () => {
     const person = persons.find(p => p.id === id)
     if (window.confirm(`Delete ${person.name}?`)) {
      personService.deleteContact(id).then(() => {
-        // filter out deleted person from list
         setPersons(persons.filter(p => p.id !== id))
      })
     }
