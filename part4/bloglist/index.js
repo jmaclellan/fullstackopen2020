0945d1f144cc5d543557d2c8e09c29371d126ec0
John MacLellan <jmaclellan24@gmail.com>
2020-12-19 11:07:21 -0500

    Add models/blog.js

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -5,15 +5,6 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
 const mongoUrl = 'mongodb://localhost/bloglist'
 mongoose.connect(mongoUrl, {
   useNewUrlParser: true,
