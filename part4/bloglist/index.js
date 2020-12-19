22a97540428c3db94572d7c26fb139ab0bfb6775
John MacLellan <jmaclellan24@gmail.com>
2020-12-19 14:33:04 -0500

    Cleaned up index.js

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,12 +1,11 @@
 const http = require('http')
-const express = require('express')
-
-const cors = require('cors')
 const app = require('./app') // actual Express app
+
+const config = require('./utils/config')
 
 const server = http.createServer(app)
 
-const PORT = 3003
-app.listen(PORT, () => {
+const { PORT } = config
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
