c82d9738866e817d967df947a32df2286643970f
John MacLellan <jmaclellan24@gmail.com>
2020-12-19 14:39:35 -0500

    Code for establishing connection to db in app.js

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose')
+const express = require('express')
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const app = express()
+const cors = require('cors')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+
+const mongoUrl = config.MONGODB_URI
+
 mongoose.connect(mongoUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
   useCreateIndex: true,
 })
+
+app.use(cors())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
