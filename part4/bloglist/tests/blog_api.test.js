68f1d856bce6497148663e37dc9d39c2c7feb05e
John MacLellan <jmaclellan24@gmail.com>
2020-12-19 22:23:45 -0500

    Update test for blogs api

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -4,11 +4,10 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+test('there are two blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(2)
 })
 
 afterAll(() => {
