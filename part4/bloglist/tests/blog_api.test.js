6746368f5a58865df290c110ffba48d4f971b13a
John MacLellan <jmaclellan24@gmail.com>
2020-12-20 07:52:49 -0500

    Update test

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -4,10 +4,10 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test('there are two blogs', async () => {
+test('get correct number of blogs', async () => {
   const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(2)
+  expect(response.body).toHaveLength(0)
 })
 
 afterAll(() => {
