d13270d3aa84ff057b50bdb0338295b61925c5f9
John MacLellan <jmaclellan24@gmail.com>
2020-12-20 12:29:16 -0500

    Update blog api test file

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('../utils/list_helper')
 const app = require('../app')
 
 const api = supertest(app)
 
+const Blog = require('../models/blog')
+
 test('get correct number of blogs', async () => {
   const response = await api.get('/api/blogs')
 
-  expect(response.body).toStrictEqual({})
+  expect(response.body.length).toBe(helper.initialBlogs.length)
 })
 
 afterAll(() => {
