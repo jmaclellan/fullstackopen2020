d692a10a8d23e6b6efece4a51b1cda1885af58a0
John MacLellan <jmaclellan24@gmail.com>
2020-12-21 18:48:53 -0500

    Added users POST route to create new user

---
+++ part4/bloglist/controllers/users.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.post('/', async (request, response) => {
+  const { password, username, name } = request.body
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  response.json(savedUser)
+})
+
+module.exports = usersRouter
