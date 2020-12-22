73e2253692ba96b5f37ed7c4097a50e9043419e4
John MacLellan <jmaclellan24@gmail.com>
2020-12-21 20:07:01 -0500

    Bring in user and login routes into app

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,7 +4,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const config = require('./utils/config')
+const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
+const loginRouter = require('./controllers/login')
 
 const mongoUrl = config.MONGODB_URI
 
@@ -17,6 +19,9 @@ mongoose.connect(mongoUrl, {
 
 app.use(cors())
 app.use(express.json())
+
+app.use('/api/user', usersRouter)
 app.use('/api/blogs', blogsRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
