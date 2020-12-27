63df524b3c2ca01b70f7da5a7b9c1177ded82909
John MacLellan <jmaclellan24@gmail.com>
2020-12-27 09:38:38 -0500

    Validation for creating new user

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -10,6 +10,24 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const { password, username, name } = request.body
 
+  if (!username || !password) {
+    return response.status(400).json({
+      error: 'Username and password required',
+    })
+  }
+
+  if (username.length < 3 || password.length < 3) {
+    return response.status(400).json({
+      error: 'Username and password must be at least 3 characters',
+    })
+  }
+
+  if (User.find(username)) {
+    return response.status(400).json({
+      error: 'Username must be unique',
+    })
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
