97c4fc9061b9a26b1c2ff609e887208b1649c5c2
John MacLellan <jmaclellan24@gmail.com>
2020-12-27 14:59:22 -0500

    Token extractor to middleware

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const config = require('./utils/config')
+const middleware = require('./utils/middleware')
 const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
 const loginRouter = require('./controllers/login')
@@ -22,6 +23,8 @@ mongoose
 
 app.use(cors())
 app.use(express.json())
+
+app.use(middleware.tokenExtractor)
 
 app.use('/api/user', usersRouter)
 app.use('/api/blogs', blogsRouter)
