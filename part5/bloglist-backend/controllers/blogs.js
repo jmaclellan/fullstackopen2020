3381ac820aff2270c0d5603821690260b5bb2275
John MacLellan <jmaclellan24@gmail.com>
2020-12-28 20:35:59 -0500

    Moved bloglist backend into part 5 directory

---
+++ part5/bloglist-backend/controllers/blogs.js
@@ -0,0 +1,63 @@
+const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
+const Blog = require('../models/blog')
+const User = require('../models/user')
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
+})
+
+blogsRouter.post('/', async (request, response, next) => {
+  const { url, title, author, likes } = request.body
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+
+  const blog = new Blog({
+    url,
+    title,
+    author,
+    user,
+    likes,
+  })
+
+  try {
+    const savedBlog = await blog.save()
+    response.json(savedBlog.toJSON())
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogsRouter.put('/api/blogs/:id', async (request, response, next) => {
+  const { title, author, url, likes } = request.body
+
+  const blog = {
+    title,
+    author,
+    url,
+    likes,
+  }
+
+  try {
+    await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  } catch (err) {
+    next(err)
+  }
+})
+
+module.exports = blogsRouter
---
+++ part5/bloglist-backend/tests/blog_api.test.js
@@ -0,0 +1,78 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('../utils/list_helper')
+const app = require('../app')
+
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+describe('when there are initially some blogs saved', () => {
+  test('blogs are return as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('get correct number of blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('verify blog posts have id property', async () => {
+    const response = await api.get('/api/blogs')
+
+    const id = response.body.map(r => r.id)
+    expect(id).toBeDefined()
+  })
+})
+
+describe('addition of a new blog', () => {
+  test('succeed with valid data', async () => {
+    const newBlog = {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url:
+        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  })
+})
+
+test('a blog can be deleted', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  expect(blogsAtEnd.length).toBe(helper.initialBlogs.length - 1)
+
+  const id = blogsAtEnd.map(r => r.id)
+
+  expect(id).not.toContain(blogToDelete.id)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
