508efb075e22cafd62c5e715641d6ee4c328bb06
John MacLellan <jmaclellan24@gmail.com>
2021-01-02 09:43:06 -0500

    Correct number of inputs for blog form

--- part5/bloglist-frontend/src/components/BlogForm.js
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -1,26 +1,71 @@
 import React, {useState} from 'react'
 
 const BlogForm = ({createBlog}) => {
-  const [newBlog, setNewBlog] = useState('')
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+  const [likes, setLikes] = useState('')
 
-  const handleChange = e => setNewBlog(e.target.value)
-
-  const addBlog = e => {
+  const handleAddBlog = e => {
     e.preventDefault()
     createBlog({
-
+        author,
+        title,
+        url,
+        likes,
     })
+    // reset form state
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+    setLikes('')
   }
 
   return (
     <>
       <h2>Create a new blog</h2>
 
-      <form onSubmit={addBlog}>
-      <input
-        value={newBlog}
-        onChange={handleChange}
-      />
+      <form onSubmit={handleAddBlog}>
+         <div>
+             title:
+           <input
+            id="title"
+            type="text"
+            value={title}
+            name="title"
+            onChange={({ target }) => setTitle(target.value)}
+           />
+        </div>
+         <div>
+             author:
+           <input
+            id="author"
+            type="text"
+            value={author}
+            name="author"
+            onChange={({ target }) => setAuthor(target.value)}
+           />
+        </div>
+         <div>
+             url:
+           <input
+            id="url"
+            type="text"
+            value={url}
+            name="url"
+            onChange={({ target }) => setUrl(target.value)}
+           />
+        </div>
+         <div>
+             likes:
+           <input
+            id="likes"
+            type="text"
+            value={likes}
+            name="likes"
+            onChange={({ target }) => setLikes(target.value)}
+           />
+        </div>
       <button type="submit">save</button>
       </form>
     </>
