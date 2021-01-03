1d6c0ca4a8f27dc9c12750b03839ec4ad2b97758
John MacLellan <jmaclellan24@gmail.com>
2021-01-03 17:12:30 -0500

    Test that blog renders correct content

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -3,18 +3,20 @@ import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders content', () => {
+test('Blog component renders correctly', () => {
   const blog = {
     url: 'test.com',
     title: 'My Sample Blog',
     author: 'Albert Einstein',
+    like: 0
   }
 
   const component = render(
     <Blog blog={blog} />
   )
 
-  expect(component.container).toHaveTextContent(
-    'My Sample Blog'
-  )
+  expect(component.container).toHaveTextContent(blog.title)
+  expect(component.container).toHaveTextContent(blog.author)
+  expect(component.container).not.toHaveTextContent(blog.likes)
+  expect(component.container).not.toHaveTextContent(blog.url)
 })
