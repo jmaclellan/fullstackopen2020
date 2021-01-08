53261199039bc56510397d054ee295df14cce1db
John MacLellan <jmaclellan24@gmail.com>
2021-01-07 21:40:48 -0500

    Add functionality for adding anecdote

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { createAncedote } from './reducers/anecdoteReducer'
 import { useSelector, useDispatch } from 'react-redux'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
+
+  const addAnecdote = event => {
+    event.preventDefault()
+    const content = event.target.ancedote.value
+    event.target.ancedote.value = ''
+    dispatch(createAncedote(content))
+  }
 
   const vote = (id) => {
     console.log('vote', id)
@@ -24,7 +32,7 @@ const App = () => {
         </div>
       )}
       <h2>create new</h2>
-      <form>
+      <form onSubmit={addAnecdote}>
         <div><input /></div>
         <button>create</button>
       </form>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -17,13 +17,26 @@ const asObject = (anecdote) => {
   }
 }
 
+export const createAncedote = (content) => {
+  return {
+    type: 'NEW_ANCEDOTE',
+    data: {
+      content,
+      id: getId(),
+      votes: 0
+    }
+  }
+}
+
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
-
-  return state
+  switch(action.type) {
+    case 'NEW_ANCEDOTE':
+      return [...state, action.data]
+    default:
+      return state
+  }
 }
 
 export default reducer
