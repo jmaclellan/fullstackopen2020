63b84cc1e7e5c72c1c6e91e35cea8f44d4036f39
John MacLellan <jmaclellan24@gmail.com>
2021-01-07 21:43:57 -0500

    Create shell for new components

---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AnecdoteForm = () => {
+  return (
+    <form>
+      <h1>anecdote form here</h1>
+    </form>
+  )
+}
+
+export default AnecdoteForm
---
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AnecdoteList = () => {
+  return (
+    <div>
+      AnecdoteList
+    </div>
+  )
+}
+
+export default AnecdoteList
