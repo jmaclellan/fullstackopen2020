12eadfd34e0d6836ddbbcee821d8731ffbaf5985
John MacLellan <jmaclellan24@gmail.com>
2021-01-10 15:45:32 -0500

    Add start:test to package.json and add cypress tests

---
+++ part5/bloglist-frontend/cypress/integration/bloglist_app.spec.js
@@ -0,0 +1,66 @@
+describe('Blog list app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Tiger Woods',
+      username: 'twoods',
+      password: 'password123'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function() {
+    cy.contains('Login')
+  })
+
+  it('user can log in', function() {
+    cy.contains('login').click()
+    cy.get('#username').type('twoods')
+    cy.get('#password').type('password123')
+    cy.get('#login-button').click()
+
+    cy.contains('Tiger Woods logged in')
+  })
+
+  it('login fails with wrong password', function() {
+    cy.contains('login').click()
+    cy.get('#username').type('twoods')
+    cy.get('#password').type('wrong')
+    cy.get('#login-button').click()
+
+    cy.get('.error')
+      .should('contain', 'wrong credentials')
+      .and('have.css', 'color', 'rgb(255, 0, 0)')
+      .and('have.css', 'border-style', 'solid')
+    cy.get('html').should('not.contain', 'Tiger Woods logged in')
+  })
+
+  describe('when logged in', function() {
+    beforeEach(function() {
+      cy.login({ username: 'twoods', password: 'password123' })
+    })
+
+    it('a new blog can be created', function() {
+      cy.contains('new blog').click()
+      cy.get('input').type('a blog created by cypress')
+      cy.contains('save').click()
+
+      cy.contains('a blog created by cypress')
+    })
+
+    describe('and a blog exists', function () {
+      beforeEach(function () {
+        cy.createNote({ content: 'first note', important: false })
+        cy.createNote({ content: 'second note', important: false })
+        cy.createNote({ content: 'third note', important: false })
+        cy.
+      })
+
+      it('it can be made important', function () {
+        cy.contains('second note').parent().find('button').click()
+        cy.contains('second note').parent().find('button').should('contain', 'make not important')
+      })
+    })
+  })
+})
