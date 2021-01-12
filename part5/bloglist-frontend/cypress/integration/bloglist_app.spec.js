694a6bbbb66d086c64309bb291430c3488c86548
John MacLellan <jmaclellan24@gmail.com>
2021-01-12 18:17:06 -0500

    Fix test typo & add scripts to package.json

--- part5/bloglist-frontend/cypress/integration/bloglist_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/bloglist_app.spec.js
@@ -54,7 +54,6 @@ describe('Blog list app', function() {
         cy.createNote({ content: 'first note', important: false })
         cy.createNote({ content: 'second note', important: false })
         cy.createNote({ content: 'third note', important: false })
-        cy.
       })
 
       it('it can be made important', function () {
