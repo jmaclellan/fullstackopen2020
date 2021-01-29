2fa78d7a8b8fcff1d0de8b8134f3e310ad855cec
John MacLellan <jmaclellan24@gmail.com>
2021-01-29 17:36:39 -0500

    Correct routes to be closer to part 4 answers

--- part5/bloglist-backend/controllers/blogs.js
+++ part5/bloglist-backend/controllers/blogs.js
@@ -4,60 +4,68 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response, next) => {
-  const { url, title, author, likes } = request.body
+  const blog = new Blog(request.body)
 
   const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
   if (!request.token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
 
   const user = await User.findById(decodedToken.id)
 
-  const blog = new Blog({
-    url,
-    title,
-    author,
-    user,
-    likes,
+  if (!blog.url || !blog.title) {
+    return response.status(400).send({ error: 'title or url missing ' })
+  }
+
+  if (!blog.likes) {
+    blog.likes = 0
+  }
+
+  blog.user = user
+  const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+  const blog = await Blog.findById(request.params.id)
+  if (blog.user.toString() !== user.id.toString()) {
+    return response
+      .status(401)
+      .json({ error: 'only the creator can delete blogs' })
+  }
+
+  await blog.remove()
+  user.blogs = user.blogs.filter(
+    b => b.id.toString() !== request.params.id.toString()
+  )
+  await user.save()
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  const blog = request.body
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
   })
-
-  try {
-    const savedBlog = await blog.save()
-    response.json(savedBlog.toJSON())
-  } catch (error) {
-    next(error)
-  }
-})
-
-blogsRouter.delete('/:id', async (request, response, next) => {
-  try {
-    await Blog.findByIdAndRemove(request.params.id)
-    response.status(204).end()
-  } catch (error) {
-    next(error)
-  }
-})
-
-blogsRouter.put('/api/blogs/:id', async (request, response, next) => {
-  const { title, author, url, likes } = request.body
-
-  const blog = {
-    title,
-    author,
-    url,
-    likes,
-  }
-
-  try {
-    await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-  } catch (err) {
-    next(err)
-  }
+  response.json(updatedBlog.toJSON())
 })
 
 module.exports = blogsRouter
--- part5/bloglist-backend/controllers/login.js
+++ part5/bloglist-backend/controllers/login.js
@@ -4,11 +4,12 @@ const loginRouter = require('express').Router()
 const User = require('../models/user')
 
 loginRouter.post('/', async (request, response) => {
-  const { username, password } = request.body
+  const { body } = request
 
-  const user = await User.findOne({ username })
+  const user = await User.findOne({ username: body.username })
+
   const passwordCorrect =
-    user === null ? false : await bcrypt.compare(password, user.password)
+    user === null ? false : await bcrypt.compare(body.password, user.password)
 
   if (!(user && passwordCorrect)) {
     return response.status(401).json({
