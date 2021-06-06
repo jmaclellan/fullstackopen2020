6faf7ad7443ad9b8dceb37071d35fa51cf427f09
John MacLellan <jmaclellan24@gmail.com>
2021-06-06 18:32:25 -0400

    Error handler middleware in app.js

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -30,4 +30,7 @@ app.use('/api/user', usersRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/login', loginRouter)
 
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
 module.exports = app
