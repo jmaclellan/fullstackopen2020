472f09d61f05733aa9796da04c1d5fcf1e623538
John MacLellan <jmaclellan24@gmail.com>
2021-06-06 18:28:59 -0400

    Middleware error handlers

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,3 +1,30 @@
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token',
+    })
+  }
+  if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({
+      error: 'token expired',
+    })
+  }
+
+  next(error)
+}
+
 const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization')
   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
@@ -7,4 +34,8 @@ const tokenExtractor = (request, response, next) => {
   next()
 }
 
-module.exports = tokenExtractor
+module.exports = {
+  unknownEndpoint,
+  errorHandler,
+  tokenExtractor,
+}
