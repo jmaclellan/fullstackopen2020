bad84b15b1128393b3fddbb073bb2207ca4926ad
John MacLellan <jmaclellan24@gmail.com>
2021-06-06 18:37:42 -0400

    Username minlength 3 in user model

--- part4/bloglist/models/user.js
+++ part4/bloglist/models/user.js
@@ -4,6 +4,7 @@ const uniqueValidator = require('mongoose-unique-validator')
 const userSchema = mongoose.Schema({
   username: {
     type: String,
+    minlength: 3,
     unique: true,
   },
   name: String,
@@ -16,15 +17,18 @@ const userSchema = mongoose.Schema({
   ],
 })
 
+userSchema.plugin(uniqueValidator)
+
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
+    // the passwordHash should not be revealed
     delete returnedObject.passwordHash
   },
 })
 
-userSchema.plugin(uniqueValidator)
+const User = mongoose.model('User', userSchema)
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = User
