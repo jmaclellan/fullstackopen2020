c6770dc620c199d8ed69f113a9caf5a322a83735
John MacLellan <jmaclellan24@gmail.com>
2021-06-07 20:33:29 -0400

    When multiple users create blogs test

--- part5/bloglist-frontend/cypress/integration/bloglist_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/bloglist_app.spec.js
@@ -21,7 +21,6 @@ describe('Blog list app', function() {
 
   describe('Login', () => {
     it('succeeds with correct credentials', function() {
-      cy.contains('login').click()
       cy.get('#username').type('twoods')
       cy.get('#password').type('password123')
       cy.get('#login-button').click()
@@ -53,18 +52,58 @@ describe('Blog list app', function() {
       cy.contains('random blog post')
       cy.contains('Dan Abramov')
     })
+  })
 
-    describe('and a blog exists', function () {
-      beforeEach(function () {
-        cy.createBlog({ name: 'Joe Smith', url: 'test123.com', likes: 1, title: 'Bloggy McBlogface' })
-        cy.createBlog({ name: 'John Wayne', url: 'wayneindustries.com', likes: 12, title: 'John Wayne stuff' })
-        cy.createBlog({ name: 'Tom Brady', url: 'tb12.com', likes: 55, title: 'TB 12 Method'})
-      })
+  describe('When several blogs creaded by many people exist', function() {
+    beforeEach(function() {
+      cy.login({ username: 'mluukkai', password: 'salainen' })
+      cy.createBlog({ author: 'John Doe', title: 'test1', url: 'http://example.com./test1' })
+      cy.createBlog({ author: 'John Doe', title: 'test2', url: 'http://example.com./test2' })
+      cy.contains('logout').click()
+      cy.login({ username: 'Test', password: 'password' })
+      cy.createBlog({ author: 'Jane Doe', title: 'test3', url: 'http://example.com./test3' })
 
-      it('it can be made important', function () {
-        cy.contains('second note').parent().find('button').click()
-        cy.contains('second note').parent().find('button').should('contain', 'make not important')
+      cy.contains('test1').parent().parent().as('blog1')
+      cy.contains('test2').parent().parent().as('blog2')
+      cy.contains('test3').parent().parent().as('blog3')
+    })
+
+    it('Blogs can be liked', function() {
+      cy.get('@blog2').contains('view').click()
+      cy.get('@blog2').contains('like').click()
+      cy.get('@blog2').contains('likes 1')
+    })
+
+    it('they are ordered by number of likes', function() {
+      cy.get('@blog1').contains('view').click()
+      cy.get('@blog2').contains('view').click()
+      cy.get('@blog3').contains('view').click()
+      cy.get('@blog1').contains('like').as('like1')
+      cy.get('@blog2').contains('like').as('like2')
+      cy.get('@blog3').contains('like').as('like3')
+
+      cy.get('@like2').click()
+      cy.get('@like1').click()
+      cy.get('@like1').click()
+      cy.get('@like3').click()
+      cy.get('@like3').click()
+      cy.get('@like3').click()
+
+      cy.get('.blog').then(blogs => {
+        cy.wrap(blogs[0]).contains('likes 3')
+        cy.wrap(blogs[1]).contains('likes 2')
+        cy.wrap(blogs[2]).contains('likes 1')
       })
     })
+
+    it('The creator can delete a blog', function() {
+      cy.get('@blog3').contains('view').click()
+      cy.get('@blog3').contains('remove').click()
+      cy.get('home').should('not.contain', 'test3')
+
+      cy.get('@blog2').contains('view').click()
+      cy.get('@blog2').should('not.contain', 'remove')
+    })
   })
+
 })
