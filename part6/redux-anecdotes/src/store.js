f2d583b672a60a7f27ef66181a75ab9effd6c183
John MacLellan <jmaclellan24@gmail.com>
2021-06-13 14:39:34 -0400

    Add call to composeWithDevTools

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -2,7 +2,6 @@ import { createStore } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import reducer from './reducers/anecdoteReducer'
 
-// const store = createStore(reducer, composeWithDevTools)
-const store = createStore(reducer)
+const store = createStore(reducer, composeWithDevTools())
 
 export default store
