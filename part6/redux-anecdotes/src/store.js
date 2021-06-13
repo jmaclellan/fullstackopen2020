82814fcc1f941033f2e2aed315283d8cdaff8daa
John MacLellan <jmaclellan24@gmail.com>
2021-06-13 18:04:32 -0400

    Add redux thunk

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers} from 'redux'
+import { createStore, combineReducers, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
@@ -8,6 +9,8 @@ const reducer = combineReducers({
   notifications: notificationReducer
 })
 
-const store = createStore(reducer, composeWithDevTools())
+const store = createStore(reducer, composeWithDevTools(
+  applyMiddleware(thunk)
+))
 
 export default store
