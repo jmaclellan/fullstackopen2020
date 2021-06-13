6bfe8db77d07b4a3a2b6567806ac9325a363da1b
John MacLellan <jmaclellan24@gmail.com>
2021-06-13 14:29:49 -0400

    Create separate store.js + Filter component

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -2,6 +2,7 @@ import { createStore } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import reducer from './reducers/anecdoteReducer'
 
-const store = createStore(reducer, composeWithDevTools)
+// const store = createStore(reducer, composeWithDevTools)
+const store = createStore(reducer)
 
 export default store
