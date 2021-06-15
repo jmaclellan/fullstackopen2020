438e4f9aabead1a9cd9118ba904baf56d5380fc4
John MacLellan <jmaclellan24@gmail.com>
2021-06-14 20:12:25 -0400

    Add connect to App.js component

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,25 +1,25 @@
 import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
+import Filter from './components/Filter'
 import { initializeAnecdotes } from './reducers/anecdoteReducer'
-import { useDispatch } from 'react-redux'
 
-const App = () => {
-  const dispatch = useDispatch()
-
+const App = (props) => {
   useEffect(() => {
-    dispatch(initializeAnecdotes())
-  }, [dispatch])
+    props.initializeAnecdotes()
+  }, [props])
 
   return (
     <div>
       <h2>Anecdotes</h2>
       <Notification />
+      <Filter />
       <AnecdoteList />
       <AnecdoteForm />
     </div>
   )
 }
 
-export default App
+export default connect(null, { initializeAnecdotes })(App)
