fa723ecbfe6aa4eeb77147ca6cbc550a2820d76a
John MacLellan <jmaclellan24@gmail.com>
2021-06-14 20:09:09 -0400

    Connect added to Notification component

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import {useSelector} from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
+const Notification = ({ notification }) => {
 
   if (!notification) return null
 
@@ -19,4 +18,10 @@ const Notification = () => {
   )
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification,
+  }
+}
+
+export default connect(mapStateToProps)(Notification)
