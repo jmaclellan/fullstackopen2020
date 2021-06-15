fa74557c64f41ddb6040bcfe26d7394be8e6b2f0
John MacLellan <jmaclellan24@gmail.com>
2021-06-14 20:22:34 -0400

    Vote service function made into update function

--- part6/redux-anecdotes/src/services/anecdotes.js
+++ part6/redux-anecdotes/src/services/anecdotes.js
@@ -2,22 +2,20 @@ import axios from 'axios'
 
 const baseUrl = 'http://localhost:3001/anecdotes'
 
-// const getId = () => (100000 * Math.random()).toFixed(0)
-
 const getAll = async () => {
   const response = await axios.get(baseUrl)
   return response.data
 }
 
 const createNew = async (content) => {
-  const object = { content }
+  const object = { content, votes: 0 }
   const response = await axios.post(baseUrl, object)
   return response.data
 }
 
-const incrementVotes = async (id) => {
-  const response = await axios.get(`${baseUrl}/${id}/`)
-  console.log(response)
+const update = async (object) => {
+  const response = await axios.put(`${baseUrl}/${object.id}/`)
+  return response.data
 }
 
-export default { getAll, createNew, incrementVotes }
+export default { getAll, createNew, update }
