b1c129a5746d58bd2b4df6838dbca7d1fe4c001d
John MacLellan <jmaclellan24@gmail.com>
2021-06-18 05:53:03 -0400

    Move reducer to top to anecdoteReducer file

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -10,6 +10,27 @@ const asObject = (anecdote) => {
   }
 }
 
+const anecdoteReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'INIT_ANECDOTES':
+      return action.data
+    case 'NEW_ANECDOTE':
+      return [...state, action.data]
+    case 'INCREMENT_VOTE':
+      const id = action.data.id
+      const anecdoteToChange = state.find(n => n.id === id)
+      const changedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1
+      }
+      return state.map(anecdote =>
+        anecdote.id !== id ? anecdote : changedAnecdote
+      )
+    default:
+      return state
+  }
+}
+
 export const createAnecdote = content => {
   return async dispatch => {
     const newAnecdote = await anecdoteService.createNew(asObject(content))
@@ -47,27 +68,6 @@ export const initializeNotes = () => {
       type: 'INIT_ANECDOTES',
       data: anecdotes,
     })
-  }
-}
-
-const anecdoteReducer = (state = [], action) => {
-  switch(action.type) {
-    case 'INIT_ANECDOTES':
-      return action.data
-    case 'NEW_ANECDOTE':
-      return [...state, action.data]
-    case 'INCREMENT_VOTE':
-      const id = action.data.id
-      const anecdoteToChange = state.find(n => n.id === id)
-      const changedAnecdote = {
-        ...anecdoteToChange,
-        votes: anecdoteToChange.votes + 1
-      }
-      return state.map(anecdote =>
-        anecdote.id !== id ? anecdote : changedAnecdote
-      )
-    default:
-      return state
   }
 }
 
