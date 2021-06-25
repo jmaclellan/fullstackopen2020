01a5c0958b436816090a4617cadfa141b44c6eb7
John MacLellan <jmaclellan24@gmail.com>
2021-06-24 21:18:35 -0400

    Reset functionality in useField hook

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -82,6 +82,13 @@ const CreateNew = (props) => {
     })
   }
 
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -108,6 +115,7 @@ const CreateNew = (props) => {
           />
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
