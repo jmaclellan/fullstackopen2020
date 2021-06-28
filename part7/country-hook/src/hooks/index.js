ac3a68930756d5b91b372703403c5b187b407267
John MacLellan <jmaclellan24@gmail.com>
2021-06-28 18:21:55 -0400

    Separate out components and hooks

---
+++ part7/country-hook/src/hooks/index.js
@@ -0,0 +1,21 @@
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+export const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {})
+
+  return country
+}
