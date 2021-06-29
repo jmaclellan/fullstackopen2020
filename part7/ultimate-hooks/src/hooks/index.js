ac8f228f52801ce7880e2b9fc24561b60e7c6e12
John MacLellan <jmaclellan24@gmail.com>
2021-06-28 20:35:21 -0400

    Separate file for hooks

---
+++ part7/ultimate-hooks/src/hooks/index.js
@@ -0,0 +1,31 @@
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+export const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([])
+
+  // ...
+
+  const create = (resource) => {
+    // ...
+  }
+
+  const service = {
+    create
+  }
+
+  return [
+    resources, service
+  ]
+}
