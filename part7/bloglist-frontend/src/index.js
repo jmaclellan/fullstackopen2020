27cceb8cbdeacd97e4c5f974b0f869f269467851
John MacLellan <jmaclellan24@gmail.com>
2021-07-12 21:01:54 -0400

    import create store; pass store to provider

--- part7/bloglist-frontend/src/index.js
+++ part7/bloglist-frontend/src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import {Provider} from 'react-redux'
 import blogReducer from './reducers/blogReducer'
+import { createStore } from 'redux'
 
 const store = createStore(blogReducer)
 
-ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'))
