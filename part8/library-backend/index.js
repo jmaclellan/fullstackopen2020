ad07288c0d0b7b9e9e0b2337f6bb8aa6c1472eb2
John MacLellan <jmaclellan24@gmail.com>
2021-07-28 21:02:37 -0400

    allBooks query

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -2,94 +2,6 @@ const { ApolloServer, gql, UserInputError } = require('apollo-server')
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: 'afa5b6f0-344d-11e9-a414-719c6709cf3e',
-    born: 1963,
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
-    born: 1821,
-  },
-  {
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: 'afa5b6f2-344d-11e9-a414-719c6709cf3e',
-  },
-  {
-    name: 'Sandi Metz', // birthyear not known
-    id: 'afa5b6f3-344d-11e9-a414-719c6709cf3e',
-  },
-];
-
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's name in the context of the book instead of the author's id
- * However, for simplicity, we will store the author's name in connection with the book
- */
-
-const books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: 'afa5b6f4-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring'],
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: 'afa5b6f5-344d-11e9-a414-719c6709cf3e',
-    genres: ['agile', 'patterns', 'design'],
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: 'afa5de00-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring'],
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: 'afa5de01-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring', 'patterns'],
-  },
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: 'afa5de02-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring', 'design'],
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: 'afa5de03-344d-11e9-a414-719c6709cf3e',
-    genres: ['classic', 'crime'],
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: 'afa5de04-344d-11e9-a414-719c6709cf3e',
-    genres: ['classic', 'revolution'],
-  },
-]
-
 mongoose.connect(process.env.MONGODB_URI,
     { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
   .then(() => {
@@ -157,28 +69,21 @@ const resolvers = {
     // return all books with optional filters of author and
     // or genre passed as arg => return Book array
     allBooks: (root, args) => {
+      // get all books
+      let books = Book.find({}).populate('author')
       // if no filters
-      if (!args.author && !args.genre) return Book.find({})
+      if (!args.author && !args.genre) return books
+      // filter author
       if (args.author) {
-        const foundAuthor = Book.findOne({author: args.author})
-        if (foundAuthor && args.genre) {
-          Book.find({genre: args.genre})
-        }
-
+        books = books.filter(book => book.author.name === args.author)
       }
-    //   const filterAuthor = book => book.author === args.author
-    //   const filterGenre = book => book.genres.includes(args.genre)
-    //   if (args.author && args.genre) {
-    //     return books.filter(filterAuthor).filter(filterGenre)
-    //   }
-    //   if (args.author) {
-    //     return books.filter(filterAuthor)
-    //   }
-    //   if (args.genre) {
-    //     return books.filter(filterGenre)
-    //   }
-    //   return books
-    // },
+      // filter genre
+      if (args.genre) {
+        books = books.filter(book => book.genres.contains(args.genre) === true)
+      }
+
+      return books
+    },
     allAuthors: () => {
       const bookCount = {}
       for (const author of authors) {
@@ -234,7 +139,7 @@ const resolvers = {
     login: async (root, args) => {
       const user = await User.findOne({ username: args.username })
 
-      if ( !user || args.password !== password) {
+      if ( !user || args.password !== process.env.PASSWORD) {
         throw new UserInputError('wrong credentials')
       }
 
@@ -244,13 +149,23 @@ const resolvers = {
       }
 
       return { value: jwt.sign(userForToken, process.env.JWT_SECRET)}
-    }
   }
 }
+
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ reg }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.toLowerCase().startsWith('bearer ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7), process.env.JWT_SECRET
+      )
+      const currentUser = await User.findById(decodedToken.id)
+      return { currentUser }
+    }
+  }
 })
 
 server.listen().then(({ url }) => {
