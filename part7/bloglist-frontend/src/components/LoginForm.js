f1218605a97c6642531985836c1ce4254db85d75
John MacLellan <jmaclellan24@gmail.com>
2021-07-30 19:09:33 -0400

    clean up loginform component

--- part7/bloglist-frontend/src/components/LoginForm.js
+++ part7/bloglist-frontend/src/components/LoginForm.js
@@ -1,36 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import { useDispatch } from 'react-redux'
+import { login } from '../reducers/userReducer'
+import loginService from '../services/login'
+import { setNotification } from '../reducers/notificationReducer'
+import storage from '../utils/storage'
 
-const LoginForm = ({
-  handleSubmit,
-  handleUsernameChange,
-  handlePasswordChange,
-  username,
-  password
-}) => {
+const LoginForm = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const dispatch = useDispatch()
+
+  const handleLogin = async event => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+
+      setUsername('')
+      setPassword('')
+      dispatch(login(user))
+      dispatch(setNotification(`Hello ${user.name}`))
+    } catch (error) {
+      dispatch(setNotification('wrong credentials', 'error'))
+    }
+  }
+
   return (
     <div>
-      <h2>Login</h2>
-
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleLogin}>
         <>
           username
           <input
             id='username'
             value={username}
-            onChange={handleUsernameChange}
+            onChange={({target}) => setUsername(target.value)}
           />
         </>
         <>
           password
           <input
             id='password'
-            type="password"
             value={password}
-            onChange={handlePasswordChange}
+            onChange={({target}) => setPassword(target.value)}
           />
         </>
-        <button id='login-button' type='submit'>Login</button>
+        <button id='login-button'>Login</button>
       </form>
     </div>
   )
