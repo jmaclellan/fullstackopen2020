d0f8185c3c24e5b38bf3782b2f47871f83d32661
John MacLellan <jmaclellan24@gmail.com>
2021-08-04 17:30:53 -0400

    fix up User schema

--- part8/library-backend/models/user.js
+++ part8/library-backend/models/user.js
@@ -2,12 +2,19 @@ const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
 const schema = new mongoose.Schema({
-  name: {
+  username: {
     type: String,
     required: true,
     unique: true,
-    minlength: 5
+    minlength: 3
   },
+  books: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Book'
+    }
+  ]
 })
 
+schema.plugin(uniqueValidator)
 module.exports = mongoose.model('User', schema)
